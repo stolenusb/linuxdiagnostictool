9f708348f723d08943d6c0114984edc6cdc9a575
stolenusb <xxuber6@gmail.com>
2025-07-31 19:46:58 +0100

    Added hostname, kernel version and boottime

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <sstream>
+#include <fstream>
 #include <thread>
+#include <ctime>
 #include <ncurses.h>
 #include <iomanip>
 #include <cpu.h>
@@ -10,15 +12,22 @@
 
 #define ESC_BTN 27
 
+std::string getPcInfo(int info);
+void displayPcInfo(const std::string& hostname, const std::string& kernelversion, const std::string &booted);
+
 int main()
 {
-    std::cout << "[*] Initializing Tool." << std::endl;
-
     initscr();
+    start_color();
+    use_default_colors();
     noecho();
     cbreak();
     nodelay(stdscr, TRUE);
     curs_set(0);
+
+    const std::string hostname = getPcInfo(0);
+    const std::string kernelversion = getPcInfo(1);
+    const std::string booted = getPcInfo(2);
 
     CpuInfo Cpu;
     MemInfo Mem;
@@ -27,19 +36,22 @@ int main()
     
     int height = 2 + std::thread::hardware_concurrency();
     int width = 20;
-    int startY = 0;
+    int startY = 2;
     int startX = 0;
 
-    WINDOW* cpuWin = newwin(height, width, startY, startX);
+    WINDOW* uptimeWin = newwin(2, width, startY, startX);
+    WINDOW* cpuWin = newwin(height, width, startY + 2, startX);
     WINDOW* memWin = newwin(3, width, startY, startX + width);
-    WINDOW* uptimeWin = newwin(2, width, startY + 3, startX + width);
     WINDOW* netWin = newwin(4, width, startY, startX + width * 2);
     WINDOW* portsWin = newwin(2, width, startY + 4, startX + width * 2);
+
+    // PC Info
+    displayPcInfo(hostname, kernelversion, booted);
 
     while(1) {
         if(getch() == ESC_BTN)
             break;
-        
+
         // CPU
         Cpu.displayInfo(cpuWin, width);
         
@@ -62,4 +74,79 @@ int main()
     endwin();
     
     return 0;
+}
+
+std::string getPcInfo(int info)
+{
+    switch(info) {
+        case 0: {
+            std::ifstream File("/proc/sys/kernel/hostname");
+            std::string Line;
+            std::getline(File, Line);
+
+            return Line;
+        }
+
+        case 1: {
+            std::ifstream File("/proc/version");
+            std::string Line;
+            std::getline(File, Line);
+
+            std::istringstream iss(Line);
+            std::vector<std::string> fields;
+            std::string field;
+
+            while(iss >> field)
+                fields.push_back(field);
+            
+            return fields.at(2);
+        }
+
+        case 2: {
+            std::ifstream File("/proc/stat");
+            std::string Line;
+            while(std::getline(File, Line)) {
+                if(Line.rfind("btime") == 0)
+                    break;
+            }
+
+            std::istringstream iss(Line);
+            unsigned long long btime;
+            iss >> Line >> btime;
+            
+            std::time_t boottime = static_cast<time_t>(btime);
+            std::tm* timeinfo = std::localtime(&boottime);
+
+            std::ostringstream formatted;
+            formatted << std::put_time(timeinfo, "%Y-%m-%d %H:%M");
+
+            return std::string(formatted.str());
+        }
+    }
+
+    return std::string("");
+}
+
+void displayPcInfo(const std::string& hostname, const std::string& kernelversion, const std::string &booted)
+{
+    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+
+    printw("Host: ");
+    attron(COLOR_PAIR(1));
+    printw("%s", hostname.c_str());
+    attroff(COLOR_PAIR(1));
+
+    printw("      ");
+
+    printw("Kernel: ");
+    attron(COLOR_PAIR(1));
+    printw("%s", kernelversion.c_str());
+    attroff(COLOR_PAIR(1));
+
+    printw("      ");
+
+    printw("Booted: ");
+    attron(COLOR_PAIR(1));
+    printw("%s", booted.c_str());
+    attroff(COLOR_PAIR(1));
 }
